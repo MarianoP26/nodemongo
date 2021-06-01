6c5cb8fb551fea12001ef36d828c764e9e518fda
MarianoP26 <84840897+MarianoP26@users.noreply.github.com>
2021-06-01 18:03:48 +0200

    Update notes.js

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,14 +1,16 @@
+const { response } = require('express');
 const express = require('express');
 const router = express.Router();
 const Note = require('../models/Note');
+const {isAuthenticated} = require('../helpers/auth');
 
 
-
-router.get('/notes/add', (req, res) => {
+router.get('/notes/add', isAuthenticated, (req, res) => {
     res.render('notes/new-note');
 });
 
-router.post('/notes/new-note', async (req, res) => {
+//Crear nota
+router.post('/notes/new-note',isAuthenticated, async (req, res) => {
     const {title, description} = req.body;
     const errors = [];
     if(!title){
@@ -25,24 +27,55 @@ router.post('/notes/new-note', async (req, res) => {
         });
     }else{
       const newNote = new Note({title, description});
+      newNote.user = req.user.id;
       await newNote.save();
+      req.flash('success_msg', 'Note added succesfully');
       res.redirect('/notes');
     }
 });
 
-router.get('/notes', async(req, res) =>{ 
-    await Note.find().sort({date:'desc'}) 
+//Vista general de las notas
+router.get('/notes', isAuthenticated, async(req, res) =>{ 
+    await Note.find({user: req.user.id}).sort({date:'desc'}) 
     .then(data => {
-        const contexto ={
+        const context ={
             notes: data.map(data =>{
                 return{
+                    _id:data._id,
                     title:data.title,
                     description:data.description
                 }
             })
         }
-        res.render('notes/all-notes',{notes:contexto.notes})
+        res.render('notes/all-notes',{notes:context.notes})
     })
+})
+
+
+router.get('/notes/edit/:id',isAuthenticated, async (req,res) =>{
+    await Note.findById(req.params.id).then(data => {
+        const context = {
+            _id:data._id,
+            title: data.title,
+            description: data.description
+        }
+        res.render('notes/edit-note',{note: context});
+    })
+})
+
+//Editar nota
+router.put('/notes/edit-note/:id' ,isAuthenticated, async (req, res) => {
+    const {title, description} = req.body;
+    await Note.findByIdAndUpdate(req.params.id, {title,description});
+    req.flash('success_msg', 'Note updated succesfully');
+    res.redirect('/notes');
+});
+
+//Borrar nota
+router.delete('/notes/delete/:id' , isAuthenticated,async (req, res) => {
+    await Note.findByIdAndDelete(req.params.id)
+    req.flash('success_msg', 'Note deleted succesfully');
+    res.redirect('/notes');
 })
 
 module.exports = router;
